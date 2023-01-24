18de4a6d5ab34c01d4769d81da41f59e1e2d0bd5
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2023-01-24 17:59:00 +0530

    grid layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { EmailCard } from "./components/email-card";
 import { EmailBody, EmailCardList } from "./components";
 import "./styles/styles.css"
+import "./styles/app.css"
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <EmailBody />
-    </div>
-  );
+    const [showEmailBody, setShowEmailBody] = useState(false);
+
+    return (
+        <div className="App page-wr u_fx-col">
+            <header className="filter-wr u_fx-row u_fx-al-cn">
+                <h3 className="filter-heading">Filter By:</h3>
+                <button className="filter-btn">Unread</button>
+                <button className="filter-btn">Read</button>
+                <button className="filter-btn">Favorites</button>
+            </header>
+            <div className="page-content-wr page-content-grid">
+                <aside className="aside-ec-list">
+                    <EmailCardList />
+                </aside>
+                <main className="main-email-body email-body-hide email-body-visible">
+                    <EmailBody />
+                </main>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/email-card-list/index.jsx
+++ src/components/email-card-list/index.jsx
@@ -1,8 +1,9 @@
 import { EmailCard } from "..";
+import "./email-card-list.css"
 
 export const EmailCardList = () => {
     return(
-        <div className="ec-list-wr">
+        <div className="ec-list-wr u_fx-col">
         {
             Array(15).fill(0).map((item, id) => {
                 return(
